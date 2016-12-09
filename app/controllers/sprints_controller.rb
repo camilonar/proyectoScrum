880e27dc3338b1b811448fcbe12e805432cdb799
Camilo <camilonar09@gmail.com>
2016-12-09 00:21:04 -0500

    Correcciones a estadisticas y a listado de proyectos

--- app/controllers/sprints_controller.rb
+++ app/controllers/sprints_controller.rb
@@ -74,6 +74,10 @@ class SprintsController < ApplicationController
 		flash[:notice] = "Estado del Sprint cambiado exitosamente"
 	    redirect_to project_details_path(:id => params[:proyecto_id] )
 	end
+
+	def estadisticas
+		@id = params[:id]
+	end
 	#Parametros en la creacion de un sprint
 def sprint_params
       params.require(:sprint).permit(:sprint_nombre, :sprint_estado, :sprint_fechainicio,:sprint_fechafinalizacion, :proyecto_id)
